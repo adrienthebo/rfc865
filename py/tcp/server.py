3129c74f58850b697c5d83f6657ceb52727707b9
Adrien Thebo <git@somethingsinistral.net>
2015-10-24 17:10:01 -0700

    Implement per-thread connections

--- py/tcp/server.py
+++ py/tcp/server.py
@@ -1,16 +1,31 @@
 import socket
+import threading
 
 class Server:
+    """
+    Define a common interface for TCP servers.
+    """
+
+    def __init__(self, port, handler):
+        self.port = port
+        self.handler = handler
+
+    def start(self):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def run(self):
+        pass
+
+class BasicServer(Server):
     """
     Implement an extremely simple TCP server that can handle a single TCP connection
     at a time. This implementation is meant to be as stupid and as slow as possible
     to provide a worst case performance comparison against concurrent server
     implementations.
     """
-
-    def __init__(self, port, handler):
-        self.port = port
-        self.handler = handler
 
     def start(self):
         """
@@ -38,10 +53,23 @@ class Server:
         """
         while True:
             conn, addr = self.listener.accept()
+            print("Handling connection from {0}:{1}".format(addr[0], addr[1]))
             self.handle_connection(conn, addr)
 
     def handle_connection(self, conn, addr):
-        print("Handling connection from {0}:{1}".format(addr[0], addr[1]))
-        self.handler(conn)
+        self.handler(conn, addr)
         conn.shutdown(socket.SHUT_RDWR)
         conn.close()
+
+class ThreadedServer(BasicServer):
+    """
+    Implement a TCP server that handles each connection in a separate thread.
+    """
+
+    def handle_connection(self, conn, addr):
+        def func(self, conn, addr):
+            print("Handling request in thread {0}".format(threading.current_thread()))
+            super(ThreadedServer, self).handle_connection(conn, addr)
+
+        thr = threading.Thread(target=func, args=(self, conn, addr))
+        thr.start()
